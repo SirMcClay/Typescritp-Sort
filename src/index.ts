ad4f663a49285fd025f544917650423f70c82853
sirmcclay <mcclay.design@gmail.com>
2020-11-19 11:03:34 -0300

    Refactor  CharactersCollection and LinkedList to extends Sorter and test all stuff

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,14 @@ const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('XaaxyB');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('XaaxyB');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(10);
-// linkedList.add(3);
-// linkedList.add(-5);
-// linkedList.add(0);
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
-// linkedList.print();
+const linkedList = new LinkedList();
+linkedList.add(10);
+linkedList.add(3);
+linkedList.add(-5);
+linkedList.add(0);
+linkedList.sort();
+linkedList.print();
